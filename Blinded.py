89af402f3b6b057ac7e0b4b42a1e163e11b53f4c
Brandon-Banh-858 <brandon.banh1@education.nsw.gov.au>
2025-12-11 11:03:33 +1100

    m

--- Blinded.py
+++ Blinded.py
@@ -27,6 +27,10 @@ def colourText(rgb: list, text: str, transparency = 1, background = False):
             returntext += "\n"
     return returntext
 
+battles = 0
+enemiesKilled = 0
+highestHierarchy = 0
+
 
 # fmt: on
 def PhaseChange(Phase: str):
@@ -1708,9 +1712,6 @@ def CheckResearchUpgrade(Number: int):
     research -= Number
     return True
 
-battles = 0
-enemiesKilled = 0
-highestHierarchy = 0
 SevenBuff = None
 pyterm.createItem("Passives", [assets.get("WrathPassive"), assets.get("GluttonyPassive"), assets.get("DesirePassive"), assets.get("SlothPassive"), assets.get("EnvyPassive"), assets.get("PridePassive"), assets.get("GreedPassive")])
 
@@ -3427,10 +3428,10 @@ while True:
 
 
     pyterm.renderLiteralItem(str(location) + " " + str(LeftClick) + " " + str(RightClick) + " " + str(player["Effects"]), 0, 0, "bottom left", "bottom left")
-    pyterm.renderLiteralItem("1", 78, 21, "center", "center")
-    pyterm.renderLiteralItem("2", -78, -20, "center", "center")
+    # pyterm.renderLiteralItem("1", 78, 21, "center", "center")
+    # pyterm.renderLiteralItem("2", -78, -20, "center", "center")
 
-    # pyterm.renderLiteralItem("#", location[0], location[1])
+    pyterm.renderLiteralItem("#", location[0], location[1])
 #34, 3
     pyterm.renderScreen(displayDebugMessages=True,debugDisplayMessageLimit=1,debugIsdisplayMessageLimit=False)
     elapsedTime = time.perf_counter() - startTime
